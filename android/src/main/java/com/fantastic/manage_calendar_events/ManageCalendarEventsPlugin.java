1e91a9ff14a2a860f0aac099d282ec8b3f32a380
dhaval-blackjet <dhavalbhimani@blackjet.au>
2025-09-16 09:56:20 +0530

    Update ManageCalendarEventsPlugin.java
    fix register method issue

--- android/src/main/java/com/fantastic/manage_calendar_events/ManageCalendarEventsPlugin.java
+++ android/src/main/java/com/fantastic/manage_calendar_events/ManageCalendarEventsPlugin.java
@@ -22,7 +22,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * ManageCalendarEventsPlugin
@@ -66,11 +65,11 @@ public class ManageCalendarEventsPlugin implements FlutterPlugin, ActivityAware,
     /**
      * Plugin registration.
      */
-    public static void registerWith(Registrar registrar) {
-        Context context = registrar.context();
-        Activity activity = registrar.activity();
-        setup(new ManageCalendarEventsPlugin(), registrar.messenger(), activity, context);
-    }
+//    public static void registerWith(Registrar registrar) {
+//        Context context = registrar.context();
+//        Activity activity = registrar.activity();
+//        setup(new ManageCalendarEventsPlugin(), registrar.messenger(), activity, context);
+//    }
 
 
     @Override
